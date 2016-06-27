92928e22dccf4e8e7122c37c9733c65aeb000595
Mark-R-GitHub <Mark-R-GitHub@users.noreply.github.com>
2016-06-27 19:52:42 +0100

    Third Commit: Updating the Block.

--- src/main/java/me/savikin/kris/common/blocks/BlockAirPump.java
+++ src/main/java/me/savikin/kris/common/blocks/BlockAirPump.java
@@ -1,14 +1,25 @@
 package me.savikin.kris.common.blocks;
 
 import me.savikin.kris.common.blocks.te.TileEntityAirPump;
+import me.savikin.kris.common.helper.ChatHelper;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+
+import javax.annotation.Nullable;
 
 
 /**
@@ -17,7 +28,8 @@ import net.minecraft.world.World;
 
 public class BlockAirPump extends PSBlock
 {
-    public TileEntity tileEntity = getTileEntity();
+    public float capChat;
+    public float volChat;
 
     public BlockAirPump()
     {
@@ -33,8 +45,21 @@ public class BlockAirPump extends PSBlock
         return new TileEntityAirPump();
     }
 
-    public TileEntity getTileEntity() {
-        return tileEntity;
+    private TileEntityAirPump getTE(World w, BlockPos bp)
+    {
+        return (TileEntityAirPump) w.getTileEntity(bp);
     }
 
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
+        if(!worldIn.isRemote)
+        {
+            capChat = getTE(worldIn, pos).getCapacity();
+            volChat = getTE(worldIn, pos).getVolume();
+            System.out.println("Pressure Capacity:" + capChat + "bars");
+            System.out.println("Volume of Object:" + volChat + "m3");
+            return true;
+        }
+        return false;
+    }
 }
